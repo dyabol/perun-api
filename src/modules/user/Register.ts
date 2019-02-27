2c80bc7aa4930dd61161c8aa92c2c15e03659922
Jakub Hromek <dyabol@gmail.com>
2019-02-27 15:26:46 +0100

    Authorization

--- src/modules/user/Register.ts
+++ src/modules/user/Register.ts
@@ -1,10 +1,21 @@
-import { Resolver, Query, Mutation, Arg } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Arg,
+  //Authorized,
+  UseMiddleware
+} from 'type-graphql';
 import bcrypt from 'bcryptjs';
 import { User } from '../../entity/User';
 import { RegisterInput } from './register/RegisterInput';
+import { isAuth } from '../middleware/isAuth';
+import { logger } from '../middleware/logger';
 
 @Resolver()
 export class RegisterResolver {
+  //@Authorized()
+  @UseMiddleware(isAuth, logger)
   @Query(() => String)
   async hello() {
     return 'Hello world!';
